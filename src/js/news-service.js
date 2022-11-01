5f318ad241c631fb58070887bc4679176ec52bf0
Oleksandr P <goodwin009@gmail.com>
2022-11-01 09:42:10 +0200

    add urlParams

--- src/js/news-service.js
+++ src/js/news-service.js
@@ -5,24 +5,59 @@ export default class NewsApiService {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
+    this.date = this.getDate();
   }
 
   fetchArticles() {
+    const searchParams = new URLSearchParams({
+      q: this.searchQuery,
+      searchIn: 'title,content',
+      pageSize: 15,
+      page: this.page,
+      sortBy: 'relevancy  ',
+      // from: this.date,
+    });
+    /*
+const searchParams = new URLSearchParams({
+  _limit: 5,
+  _sort: "name",
+});
+
+console.log(searchParams.toString()); // "_limit=5&_sort=name"
+
+const url = `https://jsonplaceholder.typicode.com/users?${searchParams}`;
+console.log(url); // "https://jsonplaceholder.typicode.com/users?_limit=5&_sort=name"
+*/
+    console.log(this.date);
     const options = {
       headers: {
         Authorization: API_KEY,
       },
+      status: 'ok',
     };
-    const url = `${URL}/everything?q=${this.searchQuery}&searchIn=title,content&pageSize=5&page=${this.page}`;
+    const url = `${URL}/everything?${searchParams}`;
+    // const url = `${URL}/everything?q=${this.searchQuery}&searchIn=title,content&pageSize=5&page=${this.page}`;
     // const url = `${URL}/top-headlines?country=ua&${this.searchQuery}&pageSize=5&page=${this.page}`;
 
     return fetch(url, options)
-      .then(r => r.json())
+      .then(r => {
+        if (!r.ok) {
+          throw new Error('status not ok');
+        }
+        return r.json();
+      })
       .then(({ articles }) => {
         this.incrementPage();
 
         return articles;
-      });
+      })
+      .catch(e => console.log(e));
+  }
+
+  getDate() {
+    const date = new Date();
+    console.log(date.toDateString());
+    return date;
   }
 
   incrementPage() {
