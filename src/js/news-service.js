5f0916cfb08900d9dac8c5869d61bef93b065af8
Oleksandr P <goodwin009@gmail.com>
2022-11-01 16:23:42 +0200

    edit search params

--- src/js/news-service.js
+++ src/js/news-service.js
@@ -1,42 +1,29 @@
-const URL = 'https://newsapi.org/v2';
+const URL = 'https://newsapi.org/v2/everything?';
 const API_KEY = '68f7fb59d89447ca8b8d9788122c4468';
 
 export default class NewsApiService {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
-    this.date = this.getDate();
+    // this.date = this.getDate();
   }
 
   fetchArticles() {
     const searchParams = new URLSearchParams({
       q: this.searchQuery,
-      searchIn: 'title,content',
       pageSize: 15,
       page: this.page,
-      sortBy: 'relevancy  ',
+      sortBy: 'relevancy',
       // from: this.date,
     });
-    /*
-const searchParams = new URLSearchParams({
-  _limit: 5,
-  _sort: "name",
-});
-
-console.log(searchParams.toString()); // "_limit=5&_sort=name"
-
-const url = `https://jsonplaceholder.typicode.com/users?${searchParams}`;
-console.log(url); // "https://jsonplaceholder.typicode.com/users?_limit=5&_sort=name"
-*/
     console.log(this.date);
     const options = {
       headers: {
         Authorization: API_KEY,
       },
-      status: 'ok',
     };
-    const url = `${URL}/everything?${searchParams}`;
-    // const url = `${URL}/everything?q=${this.searchQuery}&searchIn=title,content&pageSize=5&page=${this.page}`;
+    const url = `${URL}${searchParams}`;
+    // const url = `${URL}q=${this.searchQuery}&searchIn=title,content&pageSize=5&page=${this.page}`;
     // const url = `${URL}/top-headlines?country=ua&${this.searchQuery}&pageSize=5&page=${this.page}`;
 
     return fetch(url, options)
